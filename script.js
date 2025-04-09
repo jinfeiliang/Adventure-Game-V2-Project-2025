cda1d1d22710245e2baca961d43565708f320894
jinfeiliang <144455501+jinfeiliang@users.noreply.github.com>
2025-04-09 20:47:17 +0000

    Fix1

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
- window.location.href = "https://www.youtube.com/watch?v=GtL1huin9EE";
+console.log("Loading")
+setTimeout(function(){
+  window.location.href = "https://www.youtube.com/watch?v=GtL1huin9EE";
+  console.log("Launch")
+}, 1000)
+
